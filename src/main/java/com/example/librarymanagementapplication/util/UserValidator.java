02aa13e5bcc564b7be493cc8908dd38bfb9251df
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-13 22:20:14 +0530

    UserValidator.java

---
+++ src/main/java/com/example/librarymanagementapplication/util/UserValidator.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 13-May-21
+ *   Time: 10:19 PM
+ *   File: UserValidtor.java
+ */
+
+package com.example.librarymanagementapplication.util;
+
+public class UserValidator {
+}
+
