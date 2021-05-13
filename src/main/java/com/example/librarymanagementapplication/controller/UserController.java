07b645e32fc2d5a2a1e47a627551a4360103419e
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-13 22:11:24 +0530

    create UserController.java

---
+++ src/main/java/com/example/librarymanagementapplication/controller/UserController.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 13-May-21
+ *   Time: 10:11 PM
+ *   File: UserController.java
+ */
+
+package com.example.librarymanagementapplication.controller;
+
+public class UserController {
+}
+
