15c2111090a354fcb1e45102227f66f612493f43
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-13 22:31:52 +0530

    add fields

--- src/main/java/com/example/librarymanagementapplication/model/IssuedBooks.java
+++ src/main/java/com/example/librarymanagementapplication/model/IssuedBooks.java
@@ -7,6 +7,24 @@
 
 package com.example.librarymanagementapplication.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
 public class IssuedBooks {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private int userId;
+    private int bookId;
+    private String status;
+
 }
 
