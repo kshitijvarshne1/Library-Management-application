9cc044c44ed99de258b89956b87f42916687a1ac
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-13 22:20:51 +0530

    create BookNotFoundException.java

---
+++ src/main/java/com/example/librarymanagementapplication/exception/BookNotFoundException.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 13-May-21
+ *   Time: 10:20 PM
+ *   File: BookNotFoundException.java
+ */
+
+package com.example.librarymanagementapplication.exception;
+
+public class BookNotFoundException {
+}
+
