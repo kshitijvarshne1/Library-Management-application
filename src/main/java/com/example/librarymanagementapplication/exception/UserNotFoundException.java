45584631c71b7670511323062c48ee32a5e69917
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-13 22:21:19 +0530

    create UserNotFoundException.java

---
+++ src/main/java/com/example/librarymanagementapplication/exception/UserNotFoundException.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 13-May-21
+ *   Time: 10:21 PM
+ *   File: UserNotFoundException.java
+ */
+
+package com.example.librarymanagementapplication.exception;
+
+public class UserNotFoundException {
+}
+
