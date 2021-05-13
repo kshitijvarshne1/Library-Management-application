d9bd118d8b7551077d4e1e2cc55f5ca38d25d43a
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-13 22:22:48 +0530

    create CustomGlobalExceptionHandle.java

---
+++ src/main/java/com/example/librarymanagementapplication/exception/CustomGlobalExceptionHandle.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 13-May-21
+ *   Time: 10:22 PM
+ *   File: CustomGlobalExceptionHandle.java
+ */
+
+package com.example.librarymanagementapplication.exception;
+
+public class CustomGlobalExceptionHandle {
+}
+
