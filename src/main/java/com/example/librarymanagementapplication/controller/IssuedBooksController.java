665478a730f9a04e3402b34d113a01914a648a1d
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-13 22:11:06 +0530

    create IssuedBooksController.java

---
+++ src/main/java/com/example/librarymanagementapplication/controller/IssuedBooksController.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 13-May-21
+ *   Time: 10:10 PM
+ *   File: IssuedBooksController.java
+ */
+
+package com.example.librarymanagementapplication.controller;
+
+public class IssuedBooksController {
+}
+
