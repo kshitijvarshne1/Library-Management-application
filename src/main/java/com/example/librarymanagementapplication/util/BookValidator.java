b6b1d00ab52a976c305da04e9f99a423816263ae
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-13 22:19:04 +0530

    create BookValidator.java

---
+++ src/main/java/com/example/librarymanagementapplication/util/BookValidator.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 13-May-21
+ *   Time: 10:18 PM
+ *   File: BookValidator.java
+ */
+
+package com.example.librarymanagementapplication.util;
+
+public class BookValidator {
+}
+
