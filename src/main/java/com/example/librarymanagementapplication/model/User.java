0ec524da7e3da8466b859f6134e38a991ba4d865
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-13 22:25:36 +0530

    add fields and id is primary key

--- src/main/java/com/example/librarymanagementapplication/model/User.java
+++ src/main/java/com/example/librarymanagementapplication/model/User.java
@@ -7,6 +7,24 @@
 
 package com.example.librarymanagementapplication.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String email;
+    private String mobile;
+
 }
 
