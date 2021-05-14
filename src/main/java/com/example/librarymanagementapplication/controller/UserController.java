2f9b0b3222df30e6f8f28eb1f3b4322ce965319a
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-14 21:13:07 +0530

    add getUser method

--- src/main/java/com/example/librarymanagementapplication/controller/UserController.java
+++ src/main/java/com/example/librarymanagementapplication/controller/UserController.java
@@ -7,6 +7,23 @@
 
 package com.example.librarymanagementapplication.controller;
 
+import com.example.librarymanagementapplication.model.User;
+import com.example.librarymanagementapplication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@RestController
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/users")
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
 }
 
