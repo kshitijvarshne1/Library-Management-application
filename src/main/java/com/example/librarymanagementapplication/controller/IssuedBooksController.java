cadb62a2124a454e28a7397465764c5f9c72f414
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-18 00:02:27 +0530

    add findAllIssuedBooks method

--- src/main/java/com/example/librarymanagementapplication/controller/IssuedBooksController.java
+++ src/main/java/com/example/librarymanagementapplication/controller/IssuedBooksController.java
@@ -7,9 +7,22 @@
 
 package com.example.librarymanagementapplication.controller;
 
+import com.example.librarymanagementapplication.model.IssuedBooks;
+import com.example.librarymanagementapplication.repository.IssuedBooksRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class IssuedBooksController {
+    @Autowired
+    IssuedBooksRepository issuedBooksRepository;
+
+    @GetMapping("/issuedBooks")
+    public List<IssuedBooks> findAllIssuedBooks(){
+        return issuedBooksRepository.findAll();
+    }
 }
 
